ccc09b1b4602c12054fe3bdedfd92ae44785943b
htmldrum <htmldrum@tutanota.com>
2017-04-20 15:25:15 +1000

    turns off canonical mode, turns off echoing

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # my-own-editor
 - http://www.viewsourcecode.org/snaptoken/kilo/01.setup.html
--
+- Terminals
+  - Raw vs cannonical mode
+    - Carriage-return-buffered input
+  - `tcsetattr`/`tcgetattr`
+    - Edit terminal features
--- kilo.c
+++ kilo.c
@@ -1,3 +1,34 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+
+void enableRawMode();
+void disableRawMode();
+
+struct termios orig_termios;
+
 int main() {
+  enableRawMode();
+
+  char c;
+  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
+}
+
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw;
+
+  tcgetattr(STDIN_FILENO, &raw);
+
+  // bit flipping the control flag struct
+  raw.c_lflag &= ~(ECHO | ICANON); //  echo -turns off each key being printed to the terminal
+                          // ICANON - turns off cannonical mode
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
